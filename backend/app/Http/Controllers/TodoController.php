b51a5d67b91518592cd4d192bb98268d36a661c8
FERNANDO\arqui <fernando.esteves@mysmartclinic.com.br>
2025-07-21 00:07:44 -0300

    fix ( Todo List - Refatoração ): Revisão e ajustes no back-end.

--- backend/app/Http/Controllers/TodoController.php
+++ backend/app/Http/Controllers/TodoController.php
@@ -12,22 +12,18 @@ class TodoController extends Controller
     {
         try {
             $todos = Todo::all();
-            $return = [
+            return response()->json([
                 'resposta' =>
                 'Tarefas carregadas com sucesso!',
                 'data' => $todos,
-                'success' => 1,
-                200
-            ];
+                'success' => 1
+            ], 200);
         } catch (\Throwable $th) {
             \Log::error('Erro ao carregar tarefas: ', ['exception' => $th]);
-            $return = [
+            return response()->json([
                 'resposta' => 'Erro ao carregar tarefas.',
-                'success' => 0,
-                500
-            ];
-        } finally {
-            return response()->json($return);
+                'success' => 0
+            ], 500);
         }
     }
 
@@ -52,22 +48,18 @@ class TodoController extends Controller
 
             $todo = Todo::create($todo);
 
-            $return = [
+            return response()->json([
                 'resposta' =>
                 'Tarefa cadastrada com sucesso!',
                 'data' => $todo,
-                'success' => 1,
-                200
-            ];
+                'success' => 1
+            ], 200);
         } catch (\Throwable $th) {
             \Log::error('Erro ao cadastrar tarefa: ', ['exception' => $th]);
-            $return = [
+            return response()->json([
                 'resposta' => 'Erro ao cadastrar tarefa.',
-                'success' => 0,
-                500
-            ];
-        } finally {
-            return response()->json($return);
+                'success' => 0
+            ], 500);
         }
     }
 
@@ -78,28 +70,23 @@ class TodoController extends Controller
 
             if ($todo) {
                 $todo->delete();
-                $return = [
+                return response()->json([
                     'resposta' => 'Tarefa deletada com sucesso!',
                     'data' => $todo,
-                    'success' => 1,
-                    200
-                ];
+                    'success' => 1
+                ], 200);
             } else {
-                $return = [
+                return response()->json([
                     'resposta' => 'Tarefa não encontrada.',
-                    'success' => 0,
-                    404
-                ];
+                    'success' => 0
+                ], 404);
             }
         } catch (\Throwable $th) {
             \Log::error('Erro ao deletar tarefa: ', ['exception' => $th]);
-            $return = [
+            return response()->json([
                 'resposta' => 'Erro ao deletar tarefa.',
-                'success' => 0,
-                500
-            ];
-        } finally {
-            return response()->json($return);
+                'success' => 0
+            ], 500);
         }
     }
 }
